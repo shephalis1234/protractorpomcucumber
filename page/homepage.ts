f8d03a4e16f3fdbd602118b0017879ec800da155
shephalis1234 <shephalis123@gmail.com>
2019-12-18 18:47:24 +0530

    mrr

--- page/homepage.ts
+++ page/homepage.ts
@@ -16,6 +16,8 @@ export class homepage extends Utilities {
     await this.selectoption(this.locatoroption,option);
     
     console.log("cucumber1");
+    console.log("cucumber2");
+    console.log("cucumber2");
   }
   
 }
